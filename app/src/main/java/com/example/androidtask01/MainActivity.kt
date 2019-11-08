8af7f34df35a9de7e853dfb484cb678eadb64f54
Gowthaman <jgowtham8@gmail.com>
2019-11-08 13:42:37 +0530

    first upload

---
+++ app/src/main/java/com/example/androidtask01/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.androidtask01
+
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.RelativeLayout
+import android.widget.ScrollView
+import android.widget.TextView
+import classes.BlurBuilder
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //text_view_id.text = "fghdddddddddddddddddddddddddj"
+//        val obj1 = BlurBuilder()
+//        val resultImg = findViewById<RelativeLayout>(R.id.backImg)
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/androidtask01/doBlur.java
@@ -0,0 +1,16 @@
+package com.example.androidtask01;
+
+import android.graphics.Bitmap;
+import android.widget.RelativeLayout;
+
+import classes.BlurBuilder;
+
+public class doBlur {
+    public void blur(){
+//        ImageView resultImage = (ImageView) findViewById(R.id.resultImage);
+//        Bitmap resultBmp = BlurBuilder.blur(this, BitmapFactory.decodeResource(getResources(), R.drawable.logo_ssaurel));
+//        resultImage.setImageBitmap(resultBmp);
+
+
+    }
+}
